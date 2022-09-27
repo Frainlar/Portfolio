8274d7aebf5903bdf2873583c6cad4e6f6dfce69
Edson Frainlar <fedson@kaartech.com>
2022-09-28 04:12:20 +0530

    content content added

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -20,6 +20,7 @@ export const IndexPageTemplate = ({
 }) => (
   <>
     <SEO />
+    <script>    (function(c,l,a,r,i,t,y){        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);    })(window, document, "clarity", "script", "dudo7yvon6");</script>
     <Hero />
     <LatestPosts />
     {/* <div className="hero is-primary is-bold">
