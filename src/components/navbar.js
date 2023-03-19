fcdfe5978dd8b14f242436d3754abdc04522f660
Edson Frainlar <fedson@kaartech.com>
2023-03-19 14:58:38 +0530

    yml bug authors

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,188 +1,216 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import { useState } from "react";
 import { Link } from "gatsby";
+import { FaCoffee, FaTwitter } from "react-icons/fa";
 import {
-  // FaCoffee,
-  FaRss,
-  FaHeart,
-  FaTwitter,
-  FaHome,
-  FaInfoCircle,
-  FaBullhorn
-} from "react-icons/fa";
+  FcContacts,
+  FcHome,
+  FcPortraitMode,
+  FcReading,
+  FcLike,
+  FcTimeline,
+  FcPuzzle,
+} from "react-icons/fc";
 import { GoMarkGithub } from "react-icons/go";
-import { OutboundLink } from "gatsby-plugin-google-analytics";
+import { OutboundLink } from "gatsby-plugin-google-gtag";
 import { StaticQuery, graphql } from "gatsby";
-// import Img from "gatsby-image";
+import { GatsbyImage } from "gatsby-plugin-image";
+import { useColorMode } from "theme-ui";
+import { FiMoon, FiSun } from "react-icons/fi";
 
-const Navbar = class extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      active: false,
-      navBarActiveClass: "",
-      hamburgerMenuClass: ""
-    };
+import NavbarItem from "@components/navbar-item";
+
+function Navbar({ logo }) {
+  const [colorMode, setColorMode] = useColorMode();
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  function toggleHamburger() {
+    setNavbarOpen((prev) => !prev);
   }
 
-  toggleHamburger = () => {
-    // toggle the active boolean in the state
-    this.setState(
-      {
-        active: !this.state.active
-      },
-      // after state has been updated,
-      () => {
-        // set the class in state for the navbar accordingly
-        this.state.active
-          ? this.setState({
-              navBarActiveClass: "is-active",
-              hamburgerMenuClass: "is-active"
-            })
-          : this.setState({
-              navBarActiveClass: "",
-              hamburgerMenuClass: ""
-            });
-      }
-    );
-  };
-
-  render() {
-    return (
-      <nav className="navbar is-spaced" id="ar-navbar">
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item">
-              {/* <figure className="image is-vertical-center">
-                <Img
-                  fluid={this.props.logo.childImageSharp.fluid}
-                  className="nav-logo"
-                />
-              </figure> */}
-              <h1 className="navbar-item">
-                <strong><i>Edson Frainlar</i></strong>
-              </h1>
-            </Link>
-            <OutboundLink
-              className="navbar-item is-hidden-desktop"
-              href="https://github.com/Frainlar/Portfolio"
-              target="_blank"
-            >
-              <span className="icon">
-                <GoMarkGithub size={24} />
-              </span>
-            </OutboundLink>
-            <OutboundLink
-              className="navbar-item is-hidden-desktop"
-              href="https://twitter.com/EdsonFrainlar"
-              target="_blank"
-            >
-              <span className="icon" style={{ color: "#55acee" }}>
-                <FaTwitter size={24} />
-              </span>
-            </OutboundLink>
-            <div
-              role="button"
-              className={`navbar-burger burger ${this.state.hamburgerMenuClass}`}
-              onClick={() => this.toggleHamburger()}
-              onKeyDown={() => this.toggleHamburger()}
-              aria-label="menu"
-              aria-expanded="false"
-              data-target="ar-navMenu"
-              tabIndex={0}
-            >
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
+  return (
+    <nav className="navbar is-spaced" id="ar-navbar">
+      <div className="navbar-brand">
+        <Link to="/" className="navbar-item">
+          <figure className="image is-vertical-center">
+            <GatsbyImage
+              image={logo.childImageSharp.gatsbyImageData}
+              alt="logo"
+              className="nav-logo"
+            />
+          </figure>
+        </Link>
+        <OutboundLink
+          className="navbar-item is-hidden-desktop"
+          href="https://github.com/abhith/abhith.net"
+          target="_blank"
+        >
+          <span className="icon">
+            <GoMarkGithub size={24} />
+          </span>
+        </OutboundLink>
+        <OutboundLink
+          className="navbar-item is-hidden-desktop"
+          href="https://twitter.com/AbhithRajan"
+          target="_blank"
+        >
+          <span className="icon" style={{ color: "#55acee" }}>
+            <FaTwitter size={24} />
+          </span>
+        </OutboundLink>
+        <div
+          className="navbar-item is-hidden-desktop"
+          sx={themeStyles.modeOption}
+        >
+          <button
+            onClick={(e) => {
+              setColorMode(colorMode === "light" ? "dark" : "light");
+            }}
+          >
+            <div sx={themeStyles.modeIcons}>
+              <div>{colorMode === "light" ? <FiMoon /> : <FiSun />}</div>
+            </div>
+          </button>
+        </div>
+        <div
+          role="button"
+          className={`navbar-burger burger ${navbarOpen ? " is-active" : ""}`}
+          onClick={() => toggleHamburger()}
+          onKeyDown={() => toggleHamburger()}
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="ar-navMenu"
+          tabIndex={0}
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </div>
+      </div>
+      <div
+        id="ar-navMenu"
+        className={`navbar-menu ${navbarOpen ? " is-active" : ""}`}
+      >
+        <div className="navbar-start">
+          <NavbarItem to="/" title="Home">
+            <FcHome />
+          </NavbarItem>
+          <NavbarItem to="/about/" title="About">
+            <FcPortraitMode />
+          </NavbarItem>
+          <NavbarItem to="/blog/" title="Blog">
+            <FcReading />
+          </NavbarItem>
+          <NavbarItem to="/snippets/" title="Snippets">
+            <FcPuzzle />
+          </NavbarItem>
+          <NavbarItem to="/recommended/" title="Recommended">
+            <FcLike />
+          </NavbarItem>
+          <NavbarItem to="/topics/" title="Topics">
+            <FcTimeline />
+          </NavbarItem>
+          <NavbarItem to="/contact/" title="Contact">
+            <FcContacts />
+          </NavbarItem>
+        </div>
+        <div className="navbar-end">
+          <div className="navbar-item is-hidden-touch">
+            <div className="field is-grouped">
+              <iframe
+                src="https://ghbtns.com/github-btn.html?user=Abhith&repo=abhith.net&type=star&count=true&size=large"
+                frameBorder="0"
+                scrolling="0"
+                width="170"
+                height="30"
+                title="Star abhith/abhith.net on GitHub"
+              ></iframe>
             </div>
           </div>
-
-          <div
-            id="ar-navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
+          <OutboundLink
+            className="navbar-item is-hidden-touch"
+            href="https://github.com/abhith/abhith.net"
+            target="_blank"
           >
-            <div className="navbar-start">
-              <Link to="/" className="navbar-item">
-                <FaHome color="green" /> <span> &nbsp;Home</span>
-              </Link>
-              <Link to="/about" className="navbar-item">
-                <FaInfoCircle color="cyan" /> <span> &nbsp;About</span>
-              </Link>
-              <Link to="/blog" className="navbar-item">
-                <span className="icon">
-                  <FaRss color={"orange"}></FaRss>
-                </span>
-                <span>Blog</span>
-              </Link>
-              <Link to="/recommended" className="navbar-item">
-                <FaHeart color="red" /> <span> &nbsp;Recommended</span>
-              </Link>
-              <Link to="/topics" className="navbar-item">
-                <FaBullhorn color="turquoise" />
-                <span> &nbsp;Topics</span>
-              </Link>
-              {/* <button className="is-bold button k-button k-secondary raised has-gradient slanted">
-                <span role="img" aria-label="heart">
-                  HAPPY NEW YEAR 20❤️20
-                </span>
-              </button> */}
-            </div>
-
-            <div className="navbar-end">
-              <OutboundLink
-                className="navbar-item is-hidden-desktop-only"
-                href="https://github.com/Frainlar/Portfolio"
-                target="_blank"
+            <span className="icon">
+              <GoMarkGithub size={24} />
+            </span>
+          </OutboundLink>
+          <OutboundLink
+            className="navbar-item is-hidden-touch"
+            href="https://twitter.com/AbhithRajan"
+            target="_blank"
+          >
+            <span className="icon" style={{ color: "#55acee" }}>
+              <FaTwitter size={24} />
+            </span>
+          </OutboundLink>
+          <div
+            className="navbar-item is-hidden-touch"
+            sx={themeStyles.modeOption}
+          >
+            <button
+              onClick={(e) => {
+                setColorMode(colorMode === "light" ? "dark" : "light");
+              }}
+            >
+              <div sx={themeStyles.modeIcons}>
+                <div>{colorMode === "light" ? <FiMoon /> : <FiSun />}</div>
+              </div>
+            </button>
+          </div>
+          <div className="navbar-item">
+            <div className="field is-grouped">
+              <Link
+                to="/donate/"
+                className="button k-button k-primary raised has-gradient slanted"
               >
-                <span className="icon">
-                  <GoMarkGithub size={24} />
+                <span>
+                  BUY ME A <FaCoffee />
                 </span>
-              </OutboundLink>
-              <OutboundLink
-                className="navbar-item is-hidden-desktop-only"
-                href="https://twitter.com/EdsonFrainlar"
-                target="_blank"
-              >
-                <span className="icon" style={{ color: "#55acee" }}>
-                  <FaTwitter size={24} />
-                </span>
-              </OutboundLink>
-
-              {/* <div className="navbar-item">
-                <div className="field is-grouped">
-                  <p className="control">
-                    <Link
-                      to="/donate"
-                      className="button k-button k-primary raised has-gradient slanted"
-                    >
-                      <span>
-                        BUY ME A <FaCoffee />
-                      </span>
-                    </Link>
-                  </p>
-                </div>
-              </div> */}
+              </Link>
             </div>
           </div>
         </div>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
+}
+
+const themeStyles = {
+  modeOption: {
+    button: {
+      fontSize: "25px",
+      bg: "transparent",
+      border: "none",
+      cursor: "pointer",
+      mt: "-5px",
+      p: "0 20px 0 0",
+      "&:hover": {
+        color: "#bea9b3",
+      },
+    },
+  },
+  modeIcons: {
+    display: "flex",
+    alignItems: "center",
+    color: "#fff",
+    mt: "10px",
+  },
 };
 
 export default () => (
   <StaticQuery
     query={graphql`
-      query {
-        logo: file(relativePath: { eq: "ef-logo.png" }) 
-        {
+      {
+        logo: file(relativePath: { eq: "abhith-logo-lg.png" }) {
           childImageSharp {
-            fluid {
-              ...GatsbyImageSharpFluid_withWebp_tracedSVG
-            } 
+            gatsbyImageData(layout: FULL_WIDTH)
           }
         }
       }
     `}
-    render={data => <Navbar logo={data.logo} />}
+    render={(data) => <Navbar logo={data.logo} />}
   />
 );
--- src/templates/blog.template.tsx
+++ src/templates/blog.template.tsx
@@ -1,38 +1,59 @@
-import BlogRoll from "@components/BlogRoll";
-import Layout from "@components/Layout";
-import Pagination from "@components/Pagination";
-import SEO from "@components/SEO";
-import Topics from "@components/Topics";
-import { graphql } from "gatsby";
+import BlogRoll from "@components/blog-roll";
+import Layout from "@components/layout";
+import Pagination from "@components/pagination";
+import Seo from "@components/seo/seo";
+import TopicCloud from "@components/topic-cloud";
+import { graphql, Link } from "gatsby";
 import React from "react";
 import normalize from "../../gatsby/data/data.normalize";
+import PageHero from "@components/page-hero";
+import BreadcrumbActive from "@components/breadcrumb-active";
 
 function BlogPage({ pageContext, data }) {
   const articles = data.articles.edges.map(normalize.local.articles);
   const { previousPagePath, nextPagePath, topics } = pageContext;
+  const pageTitle = `Blog`;
+  const subTitle = `Abhith Rajan on Programming, The Web, Open Source, .NET, The Cloud and More.`;
 
   return (
     <Layout>
-      <SEO
-        title="Blog"
-        description="Edson Frainlar on Programming, Computer Science and Information Technology, Socio-cultural Regions of India, Quantum Technologies, Book reviews and sometimes politically"
-        slug="\blog"
+      <Seo title={pageTitle} description={subTitle} slug="/blog/" />
+      <PageHero
+        title={`All Articles`}
+        subtitle={subTitle}
+        subscribeUrl={`https://www.abhith.net/blog/rss.xml`}
       />
-
       <div className="section">
         <div className="container">
           <div className="columns">
             <div className="column is-9 is-10-widescreen">
-              <h4 className="title is-4 spanborder has-text-weight-bold">
-                <span>All Articles</span>
-              </h4>
+              <div className="ar-breadcrumb is-hidden-mobile pb-5">
+                <nav className="breadcrumb" aria-label="breadcrumbs">
+                  <ul>
+                    <li>
+                      <Link to={`/`}>Home</Link>
+                    </li>
+                    <li className="is-active">
+                      <BreadcrumbActive>
+                        <a
+                          className="u-url"
+                          href={`https://www.abhith.net/blog/`}
+                        >
+                          {pageTitle}
+                        </a>
+                      </BreadcrumbActive>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
               <BlogRoll posts={articles} />
               <Pagination
                 previousPagePath={previousPagePath}
                 nextPagePath={nextPagePath}
+                kind={`blog`}
               />
             </div>
-            <Topics.Cloud topics={topics} />
+            <TopicCloud topics={topics} title="Article Topics" />
           </div>
         </div>
       </div>
@@ -71,9 +92,7 @@ export const pageQuery = graphql`
           )
           hero {
             full: childImageSharp {
-              fluid(maxWidth: 2048, quality: 100) {
-                ...GatsbyImageSharpFluid_withWebp_tracedSVG
-              }
+              gatsbyImageData(quality: 100, layout: FULL_WIDTH)
             }
           }
           commentId
