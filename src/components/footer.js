be08c29f38912770d0f6fc36e184f785d97869e7
Edson Frainlar <fedson@kaartech.com>
2023-03-21 01:15:27 +0530

    Font change

--- src/components/footer.js
+++ src/components/footer.js
@@ -123,7 +123,7 @@ function Footer() {
                     </li>
                     <li className="has-margin">
                       <iframe
-                        src="https://ghbtns.com/github-btn.html?user=Frainlar&repo=Portfolio.net&type=star&count=true&size=large"
+                        src="https://ghbtns.com/github-btn.html?user=Frainlar&repo=Portfolio&type=star&count=true&size=large"
                         frameBorder="0"
                         scrolling="0"
                         width="170"
